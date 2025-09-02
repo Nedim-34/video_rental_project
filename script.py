c3c1f07baab15b6320fb143f9985d8e9d943187f
Nedim-34 <nedim1demirel@gmail.com>
2025-09-02 17:08:49 +0200

    prep. tasks

--- script.py
+++ script.py
@@ -1,27 +1,61 @@
 """ Video Rental Project"""
 
 
-# Define Classes
+### Define Classes ###
+# Video attributes
+
+
+
+
+# Customer: attributes
+
+
+
+
+# video store
+
+
+
+
+
+### Implement Core Functions ###
+# In the VideoStore class: 1-2
+
+
+
+
+# In the VideoStore class: 3-4
+
+
+
+
+# In the VideoStore class: 5-6
+
+
+
+
+### Extensions (Group Work) ###
+# Search feature
+
+
+
+
+# Late fee system
+
+
+
+
+# Ratings system
+
+
+
+
+# Collections
 
 
 
 
 
 
-# Implement Core Functions
 
-
-
-
-
-
-# Extensions (Group Work)
-
-
-
-
-
-
-
-
-# toDo section for inviduell tasks...
+### toDo section for inviduell tasks... ###
