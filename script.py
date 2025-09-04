a535ac69aa4f2b8671e48ff585d35cc7f94b7c91
Nedim-34 <nedim1demirel@gmail.com>
2025-09-04 14:19:08 +0200

    added VideoStore class 5-6

--- script.py
+++ script.py
@@ -29,7 +29,7 @@ print(customer2)
 # video store
 class VideoStore:
     """Manages the collection of videos and customers."""
-    def __init__(self):
+    def __init__(self, video_id, customer_id):
         self.videos = {video_id}  
         self.customers = {customer_id}  
 
@@ -45,7 +45,30 @@ class VideoStore:
 
 
 # In the VideoStore class: 5-6
+    def list_available_videos(self):
+            """Shows a list of all available videos."""
+            print("\n--- Available Videos ---")
+            available_count = 0
+            for video in self.videos.values():
+                if video.available:
+                    print(f"ID: {video.video_id} | Title: {video.title} | Genre: {video.genre}")
+                    available_count += 1
+            if available_count == 0:
+                print("There are currently no videos available.")
 
+    def list_customer_videos(self, customer_id):
+        """Shows a list of videos a customer has rented."""
+        customer = self.customers.get(customer_id)
+        if not customer:
+            print(f"Error: Customer with ID {customer_id} not found.")
+            return
+
+        print(f"\n--- Videos Rented by {customer.name} ---")
+        if customer.rented_videos:
+            for video in customer.rented_videos:
+                print(f"ID: {video.video_id} | Title: {video.title} | Genre: {video.genre}")
+        else:
+            print(f"{customer.name} has not rented any videos.")
 
 
 
